7cae084466c431366af404ee004fcbb0ed9dd558
Abodawoud <yousefabodawoud@gmail.com>
2023-03-22 17:53:25 -0700

    ya sataarrrr

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,10 +9,11 @@ int main(void)
 long int  a = 1;
 long int  b = 2;
 long int  c;
+int i;
 
 printf("%ld, %ld", a, b);
 
-for (int i = 2; i < 50; i++)
+for (i = 2; i < 50; i++)
 {
 c = a + b;
 printf(", %ld", c);
