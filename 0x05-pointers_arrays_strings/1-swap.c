9e4933cc1a8f94e0a1b628ee92c3acdc303a29c8
Abodawoud <yousefabodawoud@gmail.com>
2023-03-27 13:44:00 -0700

    the egoist

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,11 +3,12 @@
  *swap_int - swap a to b
  *@a: pointer to first num
  *@b: pointer to second num
- *Return : swaped numbers 
+ *Return : swaped numbers
  */
 void swap_int(int *a, int *b)
 {
 	int swapping;
+
 	swapping = *a;
 	*a = *b;
 	*b = swapping;
