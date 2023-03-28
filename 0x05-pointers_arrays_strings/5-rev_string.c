d23374e36854141c748524775af1837e31105456
Abodawoud <yousefabodawoud@gmail.com>
2023-03-27 19:02:27 -0700

    big one

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,43 +1,28 @@
 #include "main.h"
+
 /**
  * rev_string - reverses a string
- * str_len - to get the length of the string
  * @s: string to reverse
+ *
  * Return: void
  */
+
+
 void rev_string(char *s)
 {
-	int len, i;
-	char *start, *end, temp;
+	int i = 0, j = 0;
+	char str[500];
 
-	len = str_len(s);
-	start = s;
-	end = s;
-
-	for (i = 0; i < len - 1; i++)
-		end++;
-
-	for (i = 0; i < len / 2; i++)
+	while (*(s + i))
 	{
-		temp = *end;
-		*end = *start;
-		*start = temp;
-
-		start++;
-		end--;
-	}
-	putchar('\n');
-}
-/**
- * str_len - find the length of a string
- * @ptr: pointer to the string to check
- * Return: void
-*/
-int str_len(char *ptr)
-{
-	int i = 0;
-
-	while (*(ptr + i) != '\0')
+		*(str + i) = *(s + i);
 		i++;
-	return (i);
+	}
+	i = i - 1;
+	while (i >= 0)
+	{
+		*(s + i) = *(str + j);
+		j++;
+		i--;
+	}
 }
