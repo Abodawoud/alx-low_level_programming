bba5741684689a1fb5729deeee0fe30a31633684
Abodawoud <yousefabodawoud@gmail.com>
2023-03-28 13:22:37 -0700

    second chance

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,10 +13,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int index = -1;
 
-	do {
+	index++;
+	while (*(src + index) != '\0')
+	{
+		*(dest + index) = *(src + index);
 		index++;
-		dest[index] = src[index];
-	} while (src[index] != '\0');
-
+	}
 	return (dest);
 }
