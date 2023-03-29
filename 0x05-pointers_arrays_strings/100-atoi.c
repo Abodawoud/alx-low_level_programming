4ec1482eac8cc80bad8f8c23b63802064abb27ab
Abodawoud <yousefabodawoud@gmail.com>
2023-03-28 18:43:55 -0700

    The EGOIST

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,43 +14,27 @@ int number;
 int TheGatheredNumbers = 0;
 int len = 0;
 int ThePresenceOfTheString;
+
 while (s[len] != '\0')
-{
-len++;
-}
-//printf("%d\n", len);
+	len++;
 for (i = 0; i < len; i++)
 {
 if (s[i] == '-')
-{
-++Negative;
-// printf("%d\n", Negative);
-}
+	++Negative;
 if (s[i] >= '0' && s[i] <= '9')
 {
-number = s[i] - '0' ;
-if(Negative % 2)
-{
-number = -number;
-}
+number = s[i] - '0';
+if (Negative % 2)
+	number = -number;
 TheGatheredNumbers = TheGatheredNumbers * 10 + number;
-
-//printf("%d\n", TheGatheredNumbers);
-
 ThePresenceOfTheString = 1;
-// printf("%d\n", ThePresenceOfTheString);
 if (s[i + 1] < '0' || s[i + 1] > '9')
-{
-break;
-}
+	break;
 ThePresenceOfTheString = 0;
-//printf("%d\n", ThePresenceOfTheString);
 }
 
 }
 if (ThePresenceOfTheString == 0)
-{
-return (0);
-}
+	return (0);
 return (TheGatheredNumbers);
 }
