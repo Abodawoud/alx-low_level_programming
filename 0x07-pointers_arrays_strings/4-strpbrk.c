58832839b2ef24245494aa62d9b5d89973ee0150
Abodawoud <yousefabodawoud@gmail.com>
2023-04-04 03:16:01 +0200

    test

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*_strpbrk - function that searches a string for any of a set of bytes.
+*@s : number of bytes in the initial segment
+*@accept : bytes
+*Return: initialLength
+*/
+char *_strpbrk(char *s, char *accept)
+{
+
+}
---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*_strstr -  function that locates a substring.
+*@haystack : number of bytes in the initial segment
+*@needle : bytes
+*Return: initialLength
+*/
+char *_strstr(char *haystack, char *needle)
+{
+
+}
