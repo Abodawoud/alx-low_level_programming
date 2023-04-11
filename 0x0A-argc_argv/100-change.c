059bea38a24312c9fad90d1e10b5b6c9d381d9c1
Abodawoud <yousefabodawoud@gmail.com>
2023-04-11 03:07:26 +0200

    take points

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - program that prints the minimum number
+*of coins to make change for an amount of money
+*@argc: argument counter
+*@argv: argument vector
+*Return: 0 if no number passed, 1 if there are symbols in input
+*/
+int main(int argc, char *argv[])
+{
+	return (0);
+}
