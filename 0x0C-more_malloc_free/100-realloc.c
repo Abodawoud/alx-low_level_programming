1f00c90cfa3ac7c889ba9905225c0f3641a8c4ef
Abodawoud <yousefabodawoud@gmail.com>
2023-04-14 00:02:51 +0200

    pushh

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,10 +15,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	if (ptr == NULL)
-		ptr = malloc(new_size);
 	if (new_size == old_size)
 		return (ptr);
+	if (ptr == NULL)
+		ptr = malloc(new_size);
 	/**
 	 * new_size > old_size or new_size < old_size
 	 * free the previous malloc
