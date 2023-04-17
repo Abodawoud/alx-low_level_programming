1f418dc74e0332e1b1e6385a15dccc8094ed251d
Abodawoud <yousefabodawoud@gmail.com>
2023-04-17 18:14:15 +0200

    file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,8 +3,8 @@
  * main - Entry point
  * Return: (0) Success
 */
-int main(int argc, char const *argv[])
+int main(void)
 {
-    printf("%s\n", __FILE__);
-    return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
