45b5530931bcb7b10379fa6357fdeef58c692a59
Abodawoud <yousefabodawoud@gmail.com>
2023-05-03 01:44:34 +0200

    try NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	i = 0;
 	while (i < idx - 1)
 	{
-		if ((*head) == NULL)
+		if ((*head) == NULL || (*head)->next == NULL)
 			return (NULL);
 		h = h->next;
 		i++;
