77a60ffa3ce8e0ada8450bf94df69e48e6593fbb
Abodawoud <yousefabodawoud@gmail.com>
2023-05-03 01:29:59 +0200

    try

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,9 +1,11 @@
 #include "lists.h"
 /**
- * get_nodeint_at_index - unction that deletes the head node of a
- * listint_t linked list
+ * insert_nodeint_at_index -  function that inserts a new node at
+ * a given position.
  * @head: pointer to struct
- * Return: the head node’s data (n).
+ * @idx: index of the list
+ * @n: pointer to struct
+ * Return: the address of the new node
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
@@ -22,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 	}
 
-	if (!h)
+	if (!h && idx != 0)
 		return (NULL);
 	p = malloc(sizeof(listint_t));
 	if (!p)
