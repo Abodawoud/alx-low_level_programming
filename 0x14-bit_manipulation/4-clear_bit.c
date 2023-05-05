d68f4ad0340e6e8f158995b9ba23ee36a53d4c79
Abodawoud <yousefabodawoud@gmail.com>
2023-05-05 03:15:22 +0200

    try again

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,10 +6,10 @@
  * @index: is the index, starting from 0 of the bit you want to get
  * Return: 1 if it worked, or -1 if an error occurred
 */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= 64)
 		return (-1);
-    *n = *n & ~(1 << index);
-    return (1);
+	*n = *n & ~(1 << index);
+	return (1);
 }
