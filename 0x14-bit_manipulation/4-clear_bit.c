a7a972fce17b786c2664eda7314e03a136b076af
Abodawoud <yousefabodawoud@gmail.com>
2023-05-05 03:15:59 +0200

    betty

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * set_bit - function that sets the value of a bit to 0 at a given index.
+ * clear_bit - function that sets the value of a bit to 0 at a given index.
  * @n: Number
  * @index: is the index, starting from 0 of the bit you want to get
  * Return: 1 if it worked, or -1 if an error occurred
