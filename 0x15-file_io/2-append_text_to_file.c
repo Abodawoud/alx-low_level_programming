0eeadda0e1be064f56370b1e7d741090e50d82df
Abodawoud <yousefabodawoud@gmail.com>
2023-05-09 03:15:55 +0200

    try

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,8 +16,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
-
-	i = write(fd, text_content, strlen(text_content));
+	if (text_content)
+		i = write(fd, text_content, strlen(text_content));
 
 	if (i == -1)
 		return (-1);
