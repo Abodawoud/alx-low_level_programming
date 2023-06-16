1df348cfa12604532a40f334ef939a02bd5afa5d
Abodawoud <yousefabodawoud@gmail.com>
2023-06-16 22:09:57 +0300

    task 7-4

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp = temp->next;
 	}
 	temp = *h;
-	if (idx > len)
+	if (idx > len || idx == 0)
 		return (NULL);
 	new_node->n = n;
 	new_node->prev = NULL;
