99979d94372640e8c163d630a47552a4a35f05d5
Abodawoud <yousefabodawoud@gmail.com>
2023-07-08 17:03:07 +0300

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,6 +15,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *curr;
 	unsigned long int index;
 
+	if (ht == NULL)
+		return (0);
 	item = malloc(sizeof(hash_node_t));
 
 	if (!item)
