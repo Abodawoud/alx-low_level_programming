1c47e37b362d2be9d9173d1da9329b02cd9cf07e
Abodawoud <yousefabodawoud@gmail.com>
2023-10-10 13:04:53 +0300

    task 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,52 @@
+#include "search_algos.h"
+/**
+ * print_array - Prints an array of integers
+ * @array: Pointer to the array
+ * @size: Size of the array
+ */
+void print_array(int *array, size_t size)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = 0; i < size; i++)
+	{
+		printf("%d", array[i]);
+		if (i < size - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
+
+/**
+ * binary_search -  function that searches for a value in a sorted array of
+ *  integers using the Binary search algorithm
+ * @array: A pointer to the first element of the array.
+ * @size: The number of elements in array.
+ * @value: The value to search for.
+ * Return: The first index where value is located.
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t mid = 0;
+
+	if (!array)
+		return (-1);
+
+	while (left <= right)
+	{
+		mid = left + (right - left) / 2;
+		print_array(array + left, right - left + 1);
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+
+	return (-1);
+}
